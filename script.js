8e3909fd73064fe47fa78e7a02239f0a8845db78
jking0186 <62160222+jking0186@users.noreply.github.com>
2020-06-13 07:11:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -67,9 +67,10 @@ function countdown() {
     // if(remTime <= june11) {
     //    document.getElementById("answer").textContent = "Ugh, no.";
     // } else 
-    if(remTime <= oneWeek) {
-        document.getElementById("answer").textContent = "We're so close. One week to go.";
-    } else if(remTime <= june13) {
+    // if(remTime <= oneWeek) {
+    //    document.getElementById("answer").textContent = "We're so close. One week to go.";
+    // } else 
+    if(remTime <= june13) {
         document.getElementById("answer").textContent = "The anticipation is killing me.";
     } else if(remTime <= june14) {
         document.getElementById("answer").textContent = "It's a no.";
