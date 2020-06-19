c1284b24a555453431414cc5499fc09c259561dc
Joe King III <jking0186@gmail.com>
2020-06-18 23:16:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 function countdown() {
     var now = new Date();
     var eventDate = new Date("June 19, 2020 16:00:00");
-    var june3 = new Date("June 3, 2020");
-    var june4 = new Date("June 4, 2020");
-    var twoWeeks = new Date("June 5, 2020");
-    var june6 = new Date("June 6, 2020");
-    var june7 = new Date("June 7, 2020")
-    var june8 = new Date("June 8, 2020")
-    var june9 = new Date("June 9, 2020")
-    var june10 = new Date("June 10, 2020")
-    var june11 = new Date("June 11, 2020")
-    var oneWeek = new Date("June 12, 2020");
-    var june13 = new Date("June 13, 2020")
-    var june14 = new Date("June 14, 2020")
-    var daysAway = new Date("June 15, 2020");
-    var comeSoFar = new Date("June 16, 2020");
-    var canYou = new Date("June 17, 2020");
-    var oneDay = new Date("June 18, 2020");
     var hoursLeft = new Date("June 19, 2020");
     var table = document.querySelector("#table")
 
